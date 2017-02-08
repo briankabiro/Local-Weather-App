a51f6a82d0ee48b1284334027690ece6aa65a2d9
BrianLee <briankigondu@gmail.com>
2017-02-08 19:33:31 +0300

    Fully functional

--- index.js
+++ index.js
@@ -16,14 +16,32 @@ window.onload = function(){
   		var longitude = position.coords.longitude;
 		var latitude = 	position.coords.latitude;
 		var url = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&APPID=543d48a9fcbbc9faa580899563312853";
-		getWeatherData(url)
+		getWeatherData(url);
 	});
 	function getWeatherData(url){
 		$.get(url, function(data){
 			//data.sys.country, data.name - Nairobi, data.main.temp = data.main.temp - 273 degree symbo = &#8451
+			console.log(data);
 			var temperature  = Math.floor(data.main.temp - 273);
 			$("#area").text(data.name + ", " + data.sys.country);
-			$("#temperature").html(temperature + ' &#8451;');
+			var format = "Celsius";
+			var fahrenheit = Math.floor((temperature * 1.8) + 32);
+			$("#temperature").html(temperature + ' &deg; <span style="color:red;">C</span>');
+			$("#skies-are").html(data.weather[0].description);
+			var iconUrl  = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+			$("#weather-card").append("<img src="+iconUrl+">");
+			$("#weather-card").append("<p>Created by Brian Kabiro &trade;");
+			
+
+			$("#temperature").on("click", function(){
+				if(format=="Celsius"){
+					$("#temperature").html(fahrenheit + ' &deg; <span style="color:red;">F</span>');
+					format = "Fahrenheit";		
+				}else{
+					$("#temperature").html(temperature + ' &deg; <span style="color:red;">C</span>' );
+					format = "Celsius";
+				}
+			})
 		})
 	}
 
